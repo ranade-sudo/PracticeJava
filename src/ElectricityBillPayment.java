b3eba8b3bc50380d56c11518186fa9fdbffee9aa
Girija <ranade26298@gmail.com>
2025-01-31 23:09:51 +0530

    Electricity Bill Payment code

---
+++ src/ElectricityBillPayment.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ElectricityBillPayment {
+
+    public static void calculateBill(int units) {
+        double totalBill = 0;
+
+        // Calculate for the first 100 units
+        if (units <= 100)
+        {
+            totalBill = units * 0.50;
+        }
+        else if (units <= 200)
+        {
+            totalBill = (100 * 0.50) + ((units - 100) * 0.75);
+        }
+        else if (units <= 300)
+        {
+            totalBill = (100 * 0.50) + (100 * 0.75) + ((units - 200) * 1.20);
+        }
+        else
+        {
+            totalBill = (100 * 0.50) + (100 * 0.75) + (100 * 1.20) + ((units - 300) * 1.50);
+        }
+
+        System.out.println("Total electricity bill for " + units + " units is: " + totalBill + " Rs.");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter units consumed:");
+        int units = sc.nextInt();
+        calculateBill(units);
+        sc.close();
+    }
+}
--- src/LoanEligibilityCode.java
+++ src/LoanEligibilityCode.java
@@ -1,13 +1,11 @@
 import org.w3c.dom.ls.LSOutput;
-
-import java.util.Scanner;
-
 //Check if a Person is Eligible for a Loan (Based on Age, Salary, and Credit Score)
 //Take the below User info and store it into the variables:
 //Age (integer) , Salary (double or float) , Credit Score (integer)
 //Age Validation: Check if the age is a positive integer, Ensure the age is at least 18 years old, Max age can be 80.
 //Salary Validation:Check if the salary is a positive number,Define a minimum salary threshold (e.g., 30,000).
 //Credit Score Validation:Check if the credit score is a positive integer Define a minimum credit score threshold (e.g., 650) Max credit score threshold (e.g., 850).
+import java.util.Scanner;
 public class LoanEligibilityCode
 {
     public static boolean isEligibleForLoan(int age, double salary, int creditScore) {
